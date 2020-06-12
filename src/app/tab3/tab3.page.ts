1c8997bd80a860378a6659cad114b5247a749d27
Blbo <blboquatre@gmail.com>
2020-06-12 08:38:57 +0200

    update tab bar dans detail debut favoris

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {DataService} from '../services/data.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(public router: Router,
+              public dataService: DataService) {}
+
+  detailAnnonce(annonce) {
+    console.log(annonce.id);
+    const URLannonce = '/detail/' + annonce.id;
+    this.router.navigateByUrl(URLannonce);
+  }
 
 }

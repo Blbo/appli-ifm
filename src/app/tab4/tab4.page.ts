5cbad67d847ecccfa63d56119d223445c7357e50
Blbo <blboquatre@gmail.com>
2020-06-16 03:46:22 +0200

    fction dataProfil

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -18,10 +18,8 @@ export class Tab4Page implements OnInit {
   inputTel = this.dataService.dataProfil[0].tel;
   inputContrat = this.dataService.dataProfil[0].typeContrat;
   inputEmploi = this.dataService.dataProfil[0].emploiRecherche;
-  inputlocalisation = this.dataService.dataProfil[0].localisation;
+  inputLocalisation = this.dataService.dataProfil[0].localisation;
   inputDispo = this.dataService.dataProfil[0].disponibilite;
-
-
 
 
   constructor(public dataService: DataService,
@@ -37,17 +35,30 @@ export class Tab4Page implements OnInit {
   dataProfilSave() {
     console.log(this.inputNom);
     console.log(this.dataService.dataProfil[0].nom);
-    if (this.inputNom !== '' || this.inputNom !== this.dataService.dataProfil[0].nom){
+    if (this.inputNom !== '' || this.inputNom !== this.dataService.dataProfil[0].nom) {
       this.dataService.dataProfil[0].nom = this.inputNom;
     }
-    console.log('inputnom: ' + this.inputNom);
-    console.log('this.dataService.dataProfil[0].nom : ' + this.dataService.dataProfil[0].nom);
-
-    if (this.inputPrenom !== '' || this.inputPrenom !== this.dataService.dataProfil[0].prenom){
+    if (this.inputPrenom !== '' || this.inputPrenom !== this.dataService.dataProfil[0].prenom) {
       this.dataService.dataProfil[0].prenom = this.inputPrenom;
     }
-    console.log('inputprenom: ' + this.inputPrenom);
-    console.log('this.dataService.dataProfil[0].prenom : ' + this.dataService.dataProfil[0].prenom);
-  }
+    if (this.inputVille !== '' || this.inputVille !== this.dataService.dataProfil[0].ville) {
+      this.dataService.dataProfil[0].ville = this.inputVille;
+    }
+    if (this.inputEmail !== '' || this.inputEmail !== this.dataService.dataProfil[0].email) {
+      this.dataService.dataProfil[0].email = this.inputEmail;
+    }
+    if (this.inputTel !== '' || this.inputTel !== this.dataService.dataProfil[0].tel) {
+      this.dataService.dataProfil[0].tel = this.inputTel;
+    }
+    if (this.inputContrat !== '' || this.inputContrat !== this.dataService.dataProfil[0].typeContrat) {
+      this.dataService.dataProfil[0].typeContrat = this.inputContrat;
+    }
+    if (this.inputLocalisation !== '' || this.inputLocalisation !== this.dataService.dataProfil[0].localisation) {
+      this.dataService.dataProfil[0].localisation = this.inputLocalisation;
+    }
+    if (this.inputDispo !== '' || this.inputDispo !== this.dataService.dataProfil[0].disponibilite) {
+      this.dataService.dataProfil[0].disponibilite = this.inputDispo;
+    }
 
+  }
 }
